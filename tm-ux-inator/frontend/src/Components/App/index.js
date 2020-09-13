27c1f07a4b6aefc2cf231b1f72fce26f743421b7
hp77-creator <himanshu7pandey7@gmail.com>
2020-09-13 20:15:29 +0530

    Layout adding phase

---
+++ tm-ux-inator/frontend/src/Components/App/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./App.css"
+import Wall from "../Wall"
+
+
+
+class App extends React.Component{
+  render(){
+    return(
+    <Wall />
+    )
+}
+}
+
+
+export default App
---
+++ tm-ux-inator/frontend/src/Components/Wall/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Wall.css"
+
+class Wall extends React.Component{
+    render(){
+        return(
+            <div className="wallStyle">
+                <div>
+             <h1>Kem Party, Shu haal Chaal</h1>
+           </div>
+
+            </div>
+
+        )
+    }    
+}
+export default Wall
